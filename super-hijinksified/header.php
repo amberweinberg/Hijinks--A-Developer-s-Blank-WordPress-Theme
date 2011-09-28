6e231b489d107b58757044882c2748590096e9cf
Amber Weinberg <amber@amberweinberg.com>
2011-09-28 11:00:52 -0500

    Fixes
    Removed validation errors, issues with head area and default local
    caching

--- super-hijinksified/header.php
+++ super-hijinksified/header.php
@@ -1,11 +1,10 @@
+<!DOCTYPE html>
 <!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
 <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" manifest="default.appcache?v=1" lang="en"><!--<![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 
-<!DOCTYPE html>
-<html>
 	<head>
 		<meta charset="UTF-8"/>
 		
