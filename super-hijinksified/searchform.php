f036c451cff40996da1e83c46fb3384853dd97ad
Amber Weinberg <amber@amberweinberg.com>
2011-09-30 12:26:22 -0500

    Fixed search function

--- super-hijinksified/searchform.php
+++ super-hijinksified/searchform.php
@@ -1,4 +1,4 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-    <input type="text" value="Enter keywords..." onfocus="if (this.value == 'Enter keywords...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter keywords...';}" />
-    <input type="submit" value="search" />
+    <p><input type="text" class="field" name="s" id="s"  value="Enter keywords..." onfocus="if (this.value == 'Enter keywords...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter keywords...';}" />
+    <input type="submit" class="submit" name="submit" value="search" /></p>
 </form>
