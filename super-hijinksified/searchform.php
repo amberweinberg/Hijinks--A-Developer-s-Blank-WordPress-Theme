f16aafa6b0ddf886b1cc208bf1f935d3201b797b
Amber Weinberg <amber@amberweinberg.com>
2012-01-31 14:07:24 +0000

    Replaced JS text search w/ HTML5 placeholder

--- super-hijinksified/searchform.php
+++ super-hijinksified/searchform.php
@@ -1,4 +1,4 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-    <p><input type="text" class="field" name="s" id="s"  value="Enter keywords..." onfocus="if (this.value == 'Enter keywords...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter keywords...';}" />
+    <p><input type="text" class="field" name="s" id="s"  placeholder="Search" />
     <input type="submit" class="submit" name="submit" value="search" /></p>
 </form>
