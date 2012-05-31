85cf1c998eb808d29d1e5ea59e7c586ca5fac47b
Amber Weinberg <amber@amberweinberg.com>
2012-05-31 11:57:43 +0100

    Removed JS bug
    HTML5 Boilerplate's defer on scripts was causing breaking in all IEs

--- super-hijinksified/footer.php
+++ super-hijinksified/footer.php
@@ -2,8 +2,8 @@
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url');?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-		<script defer src="<?php bloginfo('template_url');?>/js/plugins.js"></script>
-		<script defer src="<?php bloginfo('template_url');?>/js/script.js"></script>
+		<script src="<?php bloginfo('template_url');?>/js/plugins.js"></script>
+		<script src="<?php bloginfo('template_url');?>/js/script.js"></script>
 		<script> // Change UA-XXXXX-X to be your site's ID
 		window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
 		Modernizr.load({
