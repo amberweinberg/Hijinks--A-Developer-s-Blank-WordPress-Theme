c08dbec9ce4f2898d7321cb44c6099333cbedd55
Amber Weinberg <amber@amberweinberg.com>
2012-06-04 11:39:34 +0100

    formatted archives page a bit better

--- super-hijinksified/archive.php
+++ super-hijinksified/archive.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		<h1><?php the_title();?></h1>
-		<?php the_content();?>
+		<h2><a title="<?php the_title();?>" href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+		<?php wpe_excerpt('wpe_excerptlength_index', 'wpe_excerptmore'); ?>
 	<?php endwhile; ?>
 	
 <?php get_sidebar(); ?>
