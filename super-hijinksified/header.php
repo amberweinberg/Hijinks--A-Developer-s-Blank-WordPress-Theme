6ccdcde027cc6d12aa0170fd64f5efc872910980
Amber Weinberg <amber@amberweinberg.com>
2016-06-07 12:55:19 -0500

    Updates
    - Updated title tag
    - Updated font awesome to latest

--- super-hijinksified/header.php
+++ super-hijinksified/header.php
@@ -4,9 +4,9 @@
 	  	<meta charset="utf-8">
 	  	<meta name="viewport" content="width=device-width, initial-scale=1">
 			
-		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('title'); ?></title>
+		<title><?php bloginfo('title'); ?> <?php wp_title();?></title>
 		
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/meanmenu.min.css">
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
