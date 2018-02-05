d22ac2b8c613ccb295955fcfd52f6388b4e164aa
Amber Weinberg <amber@amberweinberg.com>
2018-02-05 13:48:09 -0600

    Small Additions
    - Added new FontAwesome v5 SVG fonts w/ defer
    
    - Improved Mobile menu w/ dropdown options
    
    - Improved mobile menu styles, added backface-visibility to
    accessibility styles

--- super-hijinksified/footer.php
+++ super-hijinksified/footer.php
@@ -15,9 +15,9 @@
 		
 		<!--Scripts-->
 		
-		<script src="https://use.fontawesome.com/7d991d7335.js"></script>
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<script src="<?php bloginfo('template_url');?>/js/script.js"></script>
-		
+
 		<!--Warn Old Browsers To Quit Being Old-->
 		
 		<script>var $buoop = {c:2};function $buo_f(){var e = document.createElement("script");e.src = "//browser-update.org/update.min.js";document.body.appendChild(e);};try {document.addEventListener("DOMContentLoaded", $buo_f,false)}catch(e){window.attachEvent("onload", $buo_f)}</script> 
--- super-hijinksified/js/script.js
+++ super-hijinksified/js/script.js
@@ -2,7 +2,7 @@
 
 jQuery(document).ready(function () {
 	
-	// Mobile Menu
+	/************MOBILE MENU************/
 				
 	var menu = $('.mobile-menu');
 	var header = $('header');
@@ -21,19 +21,57 @@ jQuery(document).ready(function () {
 	  });
 	}
 	
-	if ($(window).width() < 851) {
-	    mobileFiltering();
-	} else {
+	function mobileDrops() {
+		
+		// Add drop links
+		
+		$('header li.menu-item-has-children').each(function() {
+			$(this).append('<a class="open-children" href="#"><i class="fa fa-chevron-down" aria-hidden="true"></i></a>');
+		});
+		
+		// On click
+		
+		$('.open-children').click( function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+			var link = $(this).parent();
+	        var status = link.hasClass('active');
+	      if(status){
+	        link.removeClass('active');
+	      }else{
+	        link.addClass('active');
+	      }
+	  });
+	}
+	
+	// Kill mobile menu
+	
+	function endMobile() {
 		menu.unbind();
 		header.removeClass('active');
+		$('header li.menu-item-has-children a').unbind();
+		$('header li.menu-item-has-children').unbind().removeClass('active');
+		
+		$('header .open-children').remove();
+
+	}
+	
+	// Active mobile menu
+	
+	if ($(window).width() < 951) {
+	    mobileFiltering();
+	    mobileDrops();
+	} else {
+		endMobile();
 	}
 	
 	$( window ).resize(function() {
-	    if($(window).width() < 851 ) {
+	    if($(window).width() < 951 ) {
 	        mobileFiltering();
+	        mobileDrops();
 	    } else {
-		    menu.unbind();
-		    header.removeClass('active');
+		    endMobile();
 	    }
 	});
 
