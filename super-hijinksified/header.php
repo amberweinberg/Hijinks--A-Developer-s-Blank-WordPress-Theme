9bec6174d00180edc528c866da70e0f87152a847
Amber Weinberg <amber@amberweinberg.com>
2018-02-07 13:17:27 -0600

    FontAwesome 6 support
    - Changes to mobile menu icons & CSS for FA5 support

--- super-hijinksified/header.php
+++ super-hijinksified/header.php
@@ -22,8 +22,8 @@
 				<a class="logo" href="<?php bloginfo('url');?>"><img src="<?php bloginfo('template_url');?>/svgs/logo.svg" alt="<?php bloginfo('title');?>"/></a>
 				
 				<a class="mobile-menu" href="#">
-					<i class="fa fa-bars" aria-hidden="true"></i>
-					<i class="fa fa-close" aria-hidden="true"></i>
+					<i class="fas fa-bars"></i>
+					<i class="fas fa-times"></i>
 					<span class="accessibility">Menu</span>
 				</a>
 				
