8e7ea5dcdad0b11947749f22563892104e7e677c
Amber Weinberg <amber@amberweinberg.com>
2018-08-01 11:37:32 -0500

    Search Updates
    -Add quotes around search results title
    - Removed old ID from searchform & replaced with class for better modularity

--- WordPress/super-hijinksified/searchform.php
+++ WordPress/super-hijinksified/searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
+<form method="get" class="searchform" action="<?php bloginfo('url'); ?>">
 	<label class="accessibility" for="s">Search</label>
     <input type="text" class="field" name="s" id="s"  placeholder="Search" />
     <input type="submit" class="submit" name="submit" value="search" />
