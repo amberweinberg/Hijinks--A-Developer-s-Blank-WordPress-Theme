bb9270f1d31400f9c454967aef2ccf3a1bfc0ff0
Amber Weinberg <amber@amberweinberg.com>
2019-02-19 12:41:56 -0600

    updated font awesome

--- WordPress/super-hijinksified/footer.php
+++ WordPress/super-hijinksified/footer.php
@@ -25,7 +25,7 @@
 		
 		<!--Scripts-->
 		
-		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+		<script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
 
 		<!--Warn Old Browsers To Quit Being Old-->
 		
